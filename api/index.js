138d1e9b025de0d3bb36e4dd62e2281e826ec34f
jyotvaghasia156-rgb <jyotvaghasia156@users.noreply.github.com>
2026-08-30 20:46:23 +0530

    fix(vercel): serve CSS/JS/assets directly from Vercel CDN, route only /api/* to serverless function

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
-const handleRequest = require('../server.js');
+const http = require('http');
+const { loadDB, saveDB } = require('../server-api.js');
+const url = require('url');
 
 module.exports = (req, res) => {
-    handleRequest(req, res);
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
+    // Proxy to the full server handler
+    const handler = require('../server.js');
+    handler(req, res);
 };
